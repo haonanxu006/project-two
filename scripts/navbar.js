2ccf7fbcddb213cdc5fb038d58a4049508f5eab5
Haonan Xu <haonan@unc.edu>
2021-04-07 12:24:10 -0400

    Completed Homepage

---
+++ scripts/navbar.js
@@ -0,0 +1,8 @@
+var toggleButton = document.querySelector(".toggle");
+var links = document.querySelector(".links");
+
+function toggle() {
+    links.classList.toggle("active");
+}
+
+toggleButton.addEventListener("click", toggle);
